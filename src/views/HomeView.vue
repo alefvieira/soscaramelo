466f252c0fb513fdf5d1e431c6f82f724aa5d082
unknown <alef_coutin@hotmail.com>
2022-12-15 14:54:44 -0300

    creado o metodo de geo localização na homeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,12 +11,56 @@
                             src="https://c.tenor.com/EjnHPIL08yUAAAAM/cachorro-caramelo.gif"
                         >
                         </v-img>
+
+                        <br>
+                        <br>
+                        <ul>
+                          <li v-for="objUser in geoLocation" :key="objUser">
+                            {{objUser}}
+                          </li>
+                        </ul>
+                        {{geoLocation.city}}
     
   </div>
 </template>
 
 <script>
+export default {
+  name: "HomeView",
+  data: () => ({
+    loading: false,
+    erro: "",
+    position: {lat: 0, lon: 0},
+    geoLocation: {}
+  
+  }),
 
+  mounted() {
+    this.buscaGeoLocalizacao()
+  },
+  methods: {
+    buscaGeoLocalizacao(){
+      // if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(this.showPosition);
+      // }
+      // else{
+      //   this.localidade = "O seu navegador não suporta Geolocalização.";
+      // }
+    },
+ 
+
+    async showPosition(position){
+      this.position.lat = position.coords.latitude;
+      this.position.lon = position.coords.longitude;
+
+
+      const req = await fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${this.position.lat}&lon=${this.position.lon}`);
+      const data = await req.json();
+      this.geoLocation = data.address
+    },
+  }
+
+};
 </script>
 
 <style scoped>
